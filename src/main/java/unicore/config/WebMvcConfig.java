e339066dc842aa480130d507b372d160fe59f1e2
hyungjunSeo <floralcustom@gmail.com>
2025-06-23 15:23:21 +0900

    tiles 처리

--- src/main/java/unicore/config/WebMvcConfig.java
+++ src/main/java/unicore/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package unicore.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 import unicore.interceptor.AuthInterceptor;
 
 @Configuration
@@ -13,12 +18,6 @@ import unicore.interceptor.AuthInterceptor;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-        // JSP 파일이 위치한 폴더와 확장자 설정
-        registry.jsp("/WEB-INF/views/", ".jsp");
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -31,5 +30,32 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")  // 모든 요청에 적용
                 .excludePathPatterns("/login", "/logout", "/static/**", "/error"); // 로그인/정적 자원 제외
+    }
+
+    // Tiles Setting
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer configurer = new TilesConfigurer();
+        configurer.setDefinitions("/WEB-INF/tiles/tiles.xml");
+        configurer.setCheckRefresh(true);
+        return configurer;
+    }
+
+    // Tiles ViewResolver (우선순위 0)
+    @Bean
+    public ViewResolver tilesViewResolver() {
+        TilesViewResolver resolver = new TilesViewResolver();
+        resolver.setOrder(0);
+        return resolver;
+    }
+
+    // JSP ViewResolver (우선순위 1)
+    @Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setOrder(1);
+        return resolver;
     }
 }
